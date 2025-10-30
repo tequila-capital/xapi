8d3d3967a69d5b6d0687de93f7e87eb002bb7ca7
Augusto Gonzalez <agonzalez@tequila.capital>
2025-10-30 12:45:23 -0600

    Tutorial 4 changes

--- tutorials/tutorial_4_execution_details.py
+++ tutorials/tutorial_4_execution_details.py
@@ -1,44 +1,42 @@
 from io import StringIO
 
 import grpc
+import numpy as np
+import pandas as pd
 import utilities_pb2 as util
 import utilities_pb2_grpc as util_grpc
-import pandas as pd
-import numpy as np
+from example_base import ExampleBase
 
 
-class ExecutionDetailsExample:
+class ExecutionDetailsExample(ExampleBase):
     def __init__(self):
-        self.server = '__SERVER__'
-        self.port = '__PORT__'
-        self.user = '__USER__'
-        self.password = '__PASSWORD__'
-        self.domain = '__DOMAIN__'
-        self.locale = '__LOCALE__'
-        #self.my_order_id = 'MyOrderId'
+        super().__init__()
 
     def run(self):
-        with open(r'roots.pem', 'rb') as f:
+        with open(self.pem_path, 'rb') as f:
             cert = f.read()
         channel = grpc.secure_channel(
             f'{self.server}:{self.port}',
-            grpc.ssl_channel_credentials(root_certificates=cert)
+            grpc.ssl_channel_credentials(root_certificates=cert),
         )
         util_stub = util_grpc.UtilityServicesStub(channel)
 
-        connect_response = util_stub.Connect(util.ConnectRequest(
-            UserName=self.user,
-            Domain=self.domain,
-            Password=self.password,
-            Locale=self.locale
-        ))
+        connect_response = util_stub.Connect(
+            util.ConnectRequest(
+                UserName=self.user,
+                Domain=self.domain,
+                Password=self.password,
+                Locale=self.locale,
+            )
+        )
         print('Connect result: ', connect_response.Response)
 
         if connect_response.Response == 'success':
-            activity_response = util_stub.GetTodaysActivityJson(util.TodaysActivityJsonRequest(
-                IncludeExchangeTradeOrder=True,
-                UserToken=connect_response.UserToken
-            ))
+            activity_response = util_stub.GetTodaysActivityJson(
+                util.TodaysActivityJsonRequest(
+                    IncludeExchangeTradeOrder=True, UserToken=connect_response.UserToken
+                )
+            )
             # Assuming activity_response.TodaysActivityJson contains the JSON string
             json_str = activity_response.TodaysActivityJson
 
@@ -48,19 +46,22 @@ class ExecutionDetailsExample:
             df = pd.read_json(json_io, convert_dates=False)
 
             if not df.empty:
-                g = df.groupby(["Symbol", "Side"]).agg(
+                g = df.groupby(['Symbol', 'Side']).agg(
                     tot_filled_vol=pd.NamedAgg(column='Volume', aggfunc='sum'),
                     num_fills=pd.NamedAgg(column='Volume', aggfunc='count'),
-                    vwap=pd.NamedAgg(column='Price', aggfunc=lambda x: np.average(x, weights=df.loc[x.index, 'Volume']))
+                    vwap=pd.NamedAgg(
+                        column='Price',
+                        aggfunc=lambda x: np.average(x, weights=df.loc[x.index, 'Volume']),
+                    ),
                 )
                 print('fills:\n', g)
 
-            disconnect_response = util_stub.Disconnect(util.DisconnectRequest(
-                UserToken=connect_response.UserToken
-            ))
+            disconnect_response = util_stub.Disconnect(
+                util.DisconnectRequest(UserToken=connect_response.UserToken)
+            )
             print('Disconnect result: ', disconnect_response.ServerResponse)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     example = ExecutionDetailsExample()
     example.run()
