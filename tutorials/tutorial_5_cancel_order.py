f36860b57b8aaeb5cb71701c9fb70d61c7d2aa9e
Augusto Gonzalez <agonzalez@tequila.capital>
2025-10-30 13:40:16 -0600

    Tutorial 5 & 6 changes

--- tutorials/tutorial_5_cancel_order.py
+++ tutorials/tutorial_5_cancel_order.py
@@ -1,45 +1,44 @@
 from io import StringIO
 
 import grpc
-import utilities_pb2 as util
-import utilities_pb2_grpc as util_grpc
 import order_pb2 as ord
 import order_pb2_grpc as ord_grpc
 import pandas as pd
+import utilities_pb2 as util
+import utilities_pb2_grpc as util_grpc
+from example_base import ExampleBase
 
 
-class CancelOrderExample:
+class CancelOrderExample(ExampleBase):
     def __init__(self):
-        self.server = '__SERVER__'
-        self.port = '__PORT__'
-        self.user = '__USER__'
-        self.password = '__PASSWORD__'
-        self.domain = '__DOMAIN__'
-        self.locale = '__LOCALE__'
+        super().__init__()
         self.my_order_id = 'MyOrderId'
 
     def run(self):
-        with open(r'roots.pem', 'rb') as f:
+        with open(self.pem_path, 'rb') as f:
             cert = f.read()
         channel = grpc.secure_channel(
             f'{self.server}:{self.port}',
-            grpc.ssl_channel_credentials(root_certificates=cert)
+            grpc.ssl_channel_credentials(root_certificates=cert),
         )
         util_stub = util_grpc.UtilityServicesStub(channel)
 
-        connect_response = util_stub.Connect(util.ConnectRequest(
-            UserName=self.user,
-            Domain=self.domain,
-            Password=self.password,
-            Locale=self.locale
-        ))
+        connect_response = util_stub.Connect(
+            util.ConnectRequest(
+                UserName=self.user,
+                Domain=self.domain,
+                Password=self.password,
+                Locale=self.locale,
+            )
+        )
         print('Connect result: ', connect_response.Response)
 
         if connect_response.Response == 'success':
-            activity_response = util_stub.GetTodaysActivityJson(util.TodaysActivityJsonRequest(
-                IncludeUserSubmitOrder=True,
-                UserToken=connect_response.UserToken
-            ))
+            activity_response = util_stub.GetTodaysActivityJson(
+                util.TodaysActivityJsonRequest(
+                    IncludeUserSubmitOrder=True, UserToken=connect_response.UserToken
+                )
+            )
             # Assuming activity_response.TodaysActivityJson contains the JSON string
             json_str = activity_response.TodaysActivityJson
 
@@ -49,23 +48,23 @@ class CancelOrderExample:
             df = pd.read_json(json_io, convert_dates=False)
 
             if not df.empty:
-                xapi_order_id = df.loc[df['OrderId'] == self.my_order_id, 'OrderId'].values[0]
+                xapi_order_id = df.loc[df['OrderTag'] == self.my_order_id, 'OrderId'].values[0]
                 print('The xAPI OrderId for my order is ', xapi_order_id)
 
                 ord_stub = ord_grpc.SubmitOrderServiceStub(channel)
-                cancel_response = ord_stub.CancelSingleOrder(ord.CancelSingleOrderRequest(
-                    OrderId=xapi_order_id,
-                    UserToken=connect_response.UserToken
-                ))
+                cancel_response = ord_stub.CancelSingleOrder(
+                    ord.CancelSingleOrderRequest(
+                        OrderId=xapi_order_id, UserToken=connect_response.UserToken
+                    )
+                )
                 print('Cancel result: ', cancel_response.ServerResponse)
 
-            disconnect_response = util_stub.Disconnect(util.DisconnectRequest(
-                UserToken=connect_response.UserToken
-            ))
+            disconnect_response = util_stub.Disconnect(
+                util.DisconnectRequest(UserToken=connect_response.UserToken)
+            )
             print('Disconnect result: ', disconnect_response.ServerResponse)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     example = CancelOrderExample()
     example.run()
-
--- tutorials/tutorial_6_market_data.py
+++ tutorials/tutorial_6_market_data.py
@@ -1,56 +1,56 @@
-import grpc
 import time
 from threading import Thread
-import utilities_pb2 as util
-import utilities_pb2_grpc as util_grpc
+
+import grpc
 import market_data_pb2 as md
 import market_data_pb2_grpc as md_grpc
+import utilities_pb2 as util
+import utilities_pb2_grpc as util_grpc
+from example_base import ExampleBase
 
 
-class MarketDataExample:
+class MarketDataExample(ExampleBase):
     def __init__(self):
-        self.server = '__SERVER__'
-        self.port = '__PORT__'
-        self.user = '__USER__'
-        self.password = '__PASSWORD__'
-        self.domain = '__DOMAIN__'
-        self.locale = '__LOCALE__'
+        super().__init__()
 
     def handle_data(self, response):
         try:
             for tick in response:
                 if tick.Trdprc1.DecimalValue == 0.0:
                     continue
-                print(f'Received market data for {tick.DispName}, Last: {tick.Trdprc1.DecimalValue}')
+                print(
+                    f'Received market data for {tick.DispName}, Last: {tick.Trdprc1.DecimalValue}'
+                )
         except Exception as e:
             print(e)
 
     def run(self):
         # Load the SSL/TLS certificates
-        with open(r'roots.pem', 'rb') as f:
+        with open(self.pem_path, 'rb') as f:
             cert = f.read()
         channel = grpc.secure_channel(
             f'{self.server}:{self.port}',
-            grpc.ssl_channel_credentials(root_certificates=cert)
+            grpc.ssl_channel_credentials(root_certificates=cert),
         )
         util_stub = util_grpc.UtilityServicesStub(channel)
 
-        connect_response = util_stub.Connect(util.ConnectRequest(
-            UserName=self.user,
-            Domain=self.domain,
-            Password=self.password,
-            Locale=self.locale
-        ))
+        connect_response = util_stub.Connect(
+            util.ConnectRequest(
+                UserName=self.user,
+                Domain=self.domain,
+                Password=self.password,
+                Locale=self.locale,
+            )
+        )
         print('Connect result: ', connect_response.Response)
 
         if connect_response.Response == 'success':
             md_stub = md_grpc.MarketDataServiceStub(channel)
-            response = md_stub.SubscribeLevel1Ticks(md.Level1MarketDataRequest(
-                Symbols=['GE'],
-                Request=True,
-                Advise=True,
-                UserToken=connect_response.UserToken
-            ))
+            response = md_stub.SubscribeLevel1Ticks(
+                md.Level1MarketDataRequest(
+                    Symbols=['GE'], Request=True, Advise=True, UserToken=connect_response.UserToken
+                )
+            )
 
             t = Thread(target=self.handle_data, args=(response,))
             t.start()
@@ -61,13 +61,12 @@ class MarketDataExample:
             response.cancel()
             t.join()
 
-            disconnect_response = util_stub.Disconnect(util.DisconnectRequest(
-                UserToken=connect_response.UserToken
-            ))
+            disconnect_response = util_stub.Disconnect(
+                util.DisconnectRequest(UserToken=connect_response.UserToken)
+            )
             print('Disconnect result: ', disconnect_response.ServerResponse)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     example = MarketDataExample()
     example.run()
-
