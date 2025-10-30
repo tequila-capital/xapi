5e5f7ac0f119dffe76216cb3950bde47c5fabc56
Augusto Gonzalez <agonzalez@tequila.capital>
2025-10-30 12:31:00 -0600

    Tutorial 2 changes

--- tutorials/tutorial_2_place_order.py
+++ tutorials/tutorial_2_place_order.py
@@ -1,58 +1,56 @@
 import grpc
-import utilities_pb2 as util
-import utilities_pb2_grpc as util_grpc
 import order_pb2 as ord
 import order_pb2_grpc as ord_grpc
+import utilities_pb2 as util
+import utilities_pb2_grpc as util_grpc
+from example_base import ExampleBase
 
 
-class PlaceOrderExample:
+class PlaceOrderExample(ExampleBase):
     def __init__(self):
-        self.server = '__SERVER__'
-        self.port = '__PORT__'
-        self.user = '__USER__'
-        self.password = '__PASSWORD__'
-        self.domain = '__DOMAIN__'
-        self.locale = '__LOCALE__'
-        self.account = '__ACCOUNT__'
-        self.route = '__ROUTE__'
+        super().__init__()
         self.my_order_id = 'MyOrderId'
 
     def run(self):
-        with open(r'roots.pem', 'rb') as f:
+        with open(self.pem_path, 'rb') as f:
             cert = f.read()
         channel = grpc.secure_channel(
             f'{self.server}:{self.port}',
-            grpc.ssl_channel_credentials(root_certificates=cert)
+            grpc.ssl_channel_credentials(root_certificates=cert),
         )
         util_stub = util_grpc.UtilityServicesStub(channel)
 
-        connect_response = util_stub.Connect(util.ConnectRequest(
-            UserName=self.user,
-            Domain=self.domain,
-            Password=self.password,
-            Locale=self.locale
-        ))
+        connect_response = util_stub.Connect(
+            util.ConnectRequest(
+                UserName=self.user,
+                Domain=self.domain,
+                Password=self.password,
+                Locale=self.locale,
+            )
+        )
         print('Connect result: ', connect_response.Response)
 
         if connect_response.Response == 'success':
             ord_stub = ord_grpc.SubmitOrderServiceStub(channel)
-            order_response = ord_stub.SubmitSingleOrder(ord.SubmitSingleOrderRequest(
-                Symbol='VOD.LSE',
-                Side='BUY',
-                Quantity=10,
-                Route=self.route,
-                Account=self.account,
-                OrderTag=self.my_order_id,
-                UserToken=connect_response.UserToken
-            ))
+            order_response = ord_stub.SubmitSingleOrder(
+                ord.SubmitSingleOrderRequest(
+                    Symbol='VOD.LSE',
+                    Side='BUY',
+                    Quantity=10,
+                    Route=self.route,
+                    Account=self.account,
+                    OrderTag=self.my_order_id,
+                    UserToken=connect_response.UserToken,
+                )
+            )
             print('Order result: ', order_response)
 
-            disconnect_response = util_stub.Disconnect(util.DisconnectRequest(
-                UserToken=connect_response.UserToken
-            ))
+            disconnect_response = util_stub.Disconnect(
+                util.DisconnectRequest(UserToken=connect_response.UserToken)
+            )
             print('Disconnect result: ', disconnect_response.ServerResponse)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     example = PlaceOrderExample()
     example.run()
